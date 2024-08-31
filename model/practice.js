5f1d63a33e19bf43ffb8fc303f65ff73656e0a55
ZAINULABIDEEN29 <zk438256@gmail.com>
2024-08-31 15:39:11 -0700

    web-app

---
+++ model/practice.js
@@ -0,0 +1,8 @@
+// const express =require('express');
+// const app=express();
+
+// app.get('/profile', async function(req,res){
+//     const{email}=req.body;
+//    const user=await userModel.findOne({email});
+//    res.render('/',{user});
+// })
